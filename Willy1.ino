a4c2b8b2e7a30fbcd1435b00ff43abac6e42632d
Ryan Boyle <rtboyle9628@eagle.fgcu.edu>
2023-11-13 20:44:40 -0500

    fixed robot never actually going into reverse state, changed variables for new robot

--- Willy1.ino
+++ Willy1.ino
@@ -4,14 +4,39 @@
 // and Servo Motor.
 // ------------------------------------------------------------
 #include <Servo.h>
+#include <IRremote.h>
 
+// pins of left motor
 const int H1A = 4;
 const int H2A = 7;
 const int H12EN = 5;
 
+// pins of right motor
 const int H3A = 8;
 const int H4A = 2;
 const int H34EN = 6;
+
+const int IR_RECEIVE_PIN = 13;// have to check for pin in robot
+byte SPEED = 95; // wheels speed
+const int TM2_COUNT = 0x64;
+
+ enum AutoState { // states when in auto mode
+  MoveForward,
+  TurnLeft,
+  TurnRight,
+  CorrectLeft,
+  CorrectRight,
+  Reverse,
+  Stop
+  }; // enum AutoState
+
+  AutoState state; 
+
+  byte forward_count = 0;
+// count variables increment for consecutive times where
+// forward/left/right is < safe value
+// put in reverse when count is high enough
+
 
 
 // define class Motor
@@ -132,254 +157,33 @@ class UltrasonicSensor {
 }; // end UltrasonicSensor
 
 
-
-// declare 2 Motor variables
-Motor leftMotor;
-Motor rightMotor;
-
-Servo servo; // servo object
-byte servo_pin = A2;
-
-byte trigger_pin = A1;
-byte echo_pin = A0;
-UltrasonicSensor sensor(trigger_pin, echo_pin); 
-// object for the ultrasonic sensor, with a trigger pin of A1
-// and an echo pin of A0
-
-byte speed = 95;
-
-enum AutoState { // states when in auto mode
-  MoveForward,
-  TurnLeft,
-  TurnRight,
-  CorrectLeft,
-  CorrectRight,
-  Reverse,
-  Stop
-  }; // enum AutoState
-
-AutoState state = MoveForward; // move forward at start
-
-void turnLeft();
-void turnRight();
-void correctLeft();
-void correctRight();
-// ^ functions to call when in corresponding state
-
-void setup()
-{
-  Serial.begin(9600);
-  
-  leftMotor = Motor(H2A,H1A,H12EN);
-  rightMotor = Motor(H3A,H4A,H34EN);
-  leftMotor.setSpeed(speed);
-  rightMotor.setSpeed(speed);
-
-  // set up servo (pin, min, and max)
-  // min and max are pulse width values
-  // used to correctly move the servo between
-  // positions 0 and 180.
-  servo.attach(servo_pin, 500, 2500);
-
-  servo.write(90); // start with head straight forward
-
-  // set up timer 2 for 9984 microseconds (~0.01 s)
-  TCNT2 = 0x64;
-  TCCR2A = 0x00; // set to normal mode
-  TCCR2B = 0x07; // set 1024 prescaler
-  TIMSK2 = (1 << TOIE2); // enable Timer Overflow Interrupt flag
-  
-  interrupts(); // turn interrupts on
-
-  leftMotor.run(Motor::MotorForward);
-  rightMotor.run(Motor::MotorForward);  
-  // ^ start off by moving forward
-}
+class Wheels{
+  Motor leftMotor;
+  Motor rightMotor;
+  byte speed;// = 95;
+  // count variables increment for consecutive times where
+  // forward/left/right is < safe value
+  // put in reverse when count is high enough
 
 
-bool auto_mode = true;
-int btn; // should be enum
-
-// automatic and manual mode
-/*
-void setup()
-{
-  Serial.begin(9600);
-  IrReceiver.begin(IR_RECEIVE_PIN, true);
-  
-  //pinMode(13, OUTPUT);
-  //pinMode(IR_RECEIVE_PIN, INPUT);
-  
-  
-  
-}
-
-// create switch statmement using the enums
-
-// main application
-void loop()
-{
-  if(IrReceiver.decode()){
-    Serial.println(IrReceiver.decodedIRData.command);
-    IrReceiver.resume();
+  public:
+   Wheels(){
     
+    leftMotor.setSpeed(speed);
+    rightMotor.setSpeed(speed);
+
+
   }
-  
-}*/
+  Wheels(Motor left, Motor right, byte s){
 
-// have a switch method to stop when change happens.
-void loop()
-{
-
-// getting the mode
-//  if(IrReceiver.decode()){
-//    if(IrReceiver.decodedIRData.command == change){
-//      auto_mode = (auto_mode == true) ? false : true; 
-//      // stop robot for a second
-//       leftMotor.run(MotorStop);
-//       rightMotor.run(MotorStop);
-//       delay(1200);
-//    }
-//    IrReceiver.resume();
-//  }
+    leftMotor = left;
+    rightMotor = right;
+    speed = s;
+    leftMotor.setSpeed(speed);
+    rightMotor.setSpeed(speed);
 
 
-// leftMotor.run(MotorStop);
-// rightMotor.run(MotorStop);
-
-if(auto_mode){// then we are in auto_mode
-  switch(state) {
-    case MoveForward:
-      moveForward();
-      break;
-    case TurnLeft:
-      turnLeft();
-      break;
-    case TurnRight:
-      turnRight();
-      break;
-    case CorrectLeft:
-      correctLeft();
-      break;
-    case CorrectRight:
-      correctRight();
-      break;
-    case Reverse:
-      reverse();
-      break;
-    case Stop:
-      stop();
-      break;
   }
-
-}else{
-
-
-    switch(btn){
-
-//      case forward: 
-//        leftMotor.run(Motor::MotorForward);
-//        rightMotor.run(Motor::MotorForward);
-
-    }
-
-
-
-}
-}
-
-byte currentAngle;
-byte left; // left measurement
-byte forward; // forward measurement
-
-const byte WALL_DISTANCE = 6; // should always be 6 inches from wall
-const byte RANGE = 1; // can be + or - 1 inch from WALL_DISTANCE
-const byte COURSE_WIDTH = 28;
-
-byte forward_count = 0;
-byte left_count = 0;
-byte right_count = 0;
-// count variables increment for consecutive times where
-// forward/left/right is < safe value
-// put in reverse when count is high enough
-
-float scan_time = 1.5;
-
-// timer interrupt occurs every 0.01s
-// decrement time variables to have 
-ISR(TIMER2_OVF_vect) {
-  if(scan_time < 0.01) // bring back to 1.5 s when 0
-  {
-    Serial.println("hi");
-    scan_time = 1.5; // actually runs every ~2.5 seconds
-    scan();
-  }
-
-  scan_time = scan_time - 0.01;
-}
-
-void scan() {
-  currentAngle = servo.read();
-  //Serial.println(currentAngle);
-  if(currentAngle == 90)
-  { // if sensor is currently facing forward,
-    // scan forward then turn left and scan
-    forward = sensor.measureInches();
-    servo.write(180);
-    delay(625); // wait for servo to turn
-    left = sensor.measureInches();
-  }
-  else
-  { // if sensor is currently facing left,
-    // scan left then turn forward and scan
-    left = sensor.measureInches();
-    servo.write(90);
-    delay(625); // wait for servo to turn
-    forward = sensor.measureInches();
-  }
-
-  if(forward < 12) // forward < 12 inches
-  {
-    forward_count++;
-    if(forward_count > 2) state = Reverse;
-    // ^ put robot in reverse if it is stuck
-    else if(left < COURSE_WIDTH) state = TurnRight;
-    // ^ if approaching forward wall and left wall exists,
-    // turn right
-    else state = TurnLeft;
-    // ^ if approaching forward wall and no close left wall,
-    // turn left
-  }
-
-  else if(left < WALL_DISTANCE - RANGE)
-  {
-    left_count++;
-    if(left_count > 2) state = Reverse;
-    // ^ put robot in reverse if it is stuck
-    else state = CorrectRight;
-    // if too close to left wall, correct to right
-  }
-
-  else if(left > WALL_DISTANCE + RANGE)
-  {
-    right_count++;
-    if(right_count > 2) state = Reverse;
-    // ^ put robot in reverse if it is stuck
-    else state = CorrectLeft;
-    // if too far from left wall, correct to left
-  }
-
-  else if((left > COURSE_WIDTH && forward > COURSE_WIDTH) ||
-          (left == 0 && forward == 0))
-  {
-    state = Stop;
-    // ^ stop if both left and forward
-    // are greater than the course width
-    // or if both are too far (returns 0)
-  }
-
-  else state = MoveForward;
-}
 
 void turnLeft() {
   speed = 95; // slow down when turning
@@ -459,15 +263,10 @@ void moveForward() {
   leftMotor.run(Motor::MotorForward);
   rightMotor.run(Motor::MotorForward);
   // ^ return speed to 100 if slowed by a turn
-
-  forward_count = 0;
-  left_count = 0;
-  right_count = 0;
-  // clear counts, robot is not stuck
 }
 
 void reverse() {
-  speed = 255;
+  speed = 115;
   leftMotor.setSpeed(speed);
   rightMotor.setSpeed(speed);
 
@@ -492,3 +291,340 @@ void stop() {
   leftMotor.run(Motor::MotorStop);
   rightMotor.run(Motor::MotorStop);
 }
+
+};
+
+
+
+
+class RemoteControl{
+
+  enum RemoteState{
+  SwitchMode,// assign given signal from remote control
+  moveForward,
+  correctLeft,
+  correctRight,
+  reverse,
+  stop
+  };
+  RemoteState state;
+  bool auto_mode;
+  Wheels wheels;
+
+  public:
+  // default constructor
+  RemoteControl(){
+    IrReceiver.begin(IR_RECEIVE_PIN, true);
+    auto_mode = true;
+    state = stop;
+  } 
+  // Contructor to set pin
+  RemoteControl(int pin, Wheels w){
+    IrReceiver.begin(pin, true);
+    auto_mode = true;
+    state = stop;
+    wheels = w;
+  } 
+
+  bool currentMode(){
+  if(IrReceiver.decode()){
+    if(IrReceiver.decodedIRData.command == SwitchMode){
+      auto_mode = !auto_mode; 
+      // stop robot for a second
+       wheels.stop();
+       delay(1200);
+       return auto_mode; 
+    }
+    IrReceiver.resume();
+  }
+
+  return auto_mode;
+  }
+
+
+  // function to update state
+  void updateState(){
+  if(IrReceiver.decode()){
+    state = IrReceiver.decodedIRData.command;
+    IrReceiver.resume();
+  }
+// Get the moveForward() and other methods from outside the class by inheritance
+  switch(state) {
+    case moveForward:
+      wheels.moveForward();
+      break;
+    case correctLeft:
+      wheels.correctLeft();
+      break;
+    case correctRight:
+      wheels.correctRight();
+      break;
+    case reverse:
+      wheels.reverse();
+      break;
+    case stop:
+      wheels.stop();
+      break;
+    }
+
+  }
+
+
+};
+
+
+
+
+// Instance of RemoteControl class
+
+
+
+// declare 2 Motor variables
+Motor leftMotor(H2A,H1A,H12EN);
+Motor rightMotor(H4A,H3A,H34EN);
+
+Wheels wheels(leftMotor, rightMotor, SPEED);
+
+RemoteControl remote(IR_RECEIVE_PIN, wheels);
+
+Servo servo; // servo object
+byte servo_pin = A2;
+
+byte trigger_pin = A1;
+byte echo_pin = A0;
+UltrasonicSensor sensor(trigger_pin, echo_pin); 
+// object for the ultrasonic sensor, with a trigger pin of A1
+// and an echo pin of A0
+
+
+
+
+/*
+void turnLeft();
+void turnRight();
+void correctLeft();
+void correctRight();*/
+// ^ functions to call when in corresponding state
+/*
+void setup()
+{
+  Serial.begin(9600);
+  
+  //leftMotor = Motor(H2A,H1A,H12EN);
+  //rightMotor = Motor(H3A,H4A,H34EN);
+  //leftMotor.setSpeed(speed);
+  //rightMotor.setSpeed(speed);
+
+  // set up servo (pin, min, and max)
+  // min and max are pulse width values
+  // used to correctly move the servo between
+  // positions 0 and 180.
+  servo.attach(servo_pin, 500, 2500);
+
+  servo.write(90); // start with head straight forward
+
+  // set up timer 2 for 9984 microseconds (~0.01 s)
+  TCNT2 = TM2_COUNT;
+  TCCR2A = 0x00; // set to normal mode
+  TCCR2B = 0x07; // set 1024 prescaler
+  TIMSK2 = (1 << TOIE2); // enable Timer Overflow Interrupt flag
+  
+  interrupts(); // turn interrupts on
+
+
+  // IR Receiver for remote control
+  //IrReceiver.begin(IR_RECEIVE_PIN, true); // this will be also initialized inside the remote class
+  //auto_mode = true; // robot starts in auto mode // used inside RemoteControl class
+
+  leftMotor.run(Motor::MotorForward);
+  rightMotor.run(Motor::MotorForward);  
+  // ^ start off by moving forward
+}
+
+*/
+//***********************************************************************
+//int btn; // should be enum
+
+// automatic and manual mode
+
+void setup()
+{
+  Serial.begin(9600);
+  IrReceiver.begin(IR_RECEIVE_PIN, true);
+  
+  //pinMode(13, OUTPUT);
+  //pinMode(IR_RECEIVE_PIN, INPUT);
+  
+  
+  
+}
+
+// create switch statmement using the enums
+
+/*
+// main application
+void loop()
+{
+  if(IrReceiver.decode()){
+    Serial.println(IrReceiver.decodedIRData.command);
+    IrReceiver.resume();
+    
+  }
+  
+}
+*/
+
+// have a switch method to stop when change happens.
+void loop()
+{
+
+  
+  // getting the mode -> auto or manual (auto: 1, manual: 0)
+  bool is_auto_mode = remote.currentMode();
+
+if(is_auto_mode){// then we are in auto_mode
+  switch(state) {
+    case MoveForward:
+      wheels.moveForward();
+      break;
+    case TurnLeft:
+      wheels.turnLeft();
+      break;
+    case TurnRight:
+      wheels.turnRight();
+      break;
+    case CorrectLeft:
+      wheels.correctLeft();
+      break;
+    case CorrectRight:
+      wheels.correctRight();
+      break;
+    case Reverse:
+      wheels.reverse();
+      break;
+    case Stop:
+      wheels.stop();
+      break;
+  }
+
+}else{// manual mode
+
+    remote.updateState();
+
+  }
+
+
+
+}
+
+
+byte currentAngle;
+byte left; // left measurement
+byte forward; // forward measurement
+
+const byte WALL_DISTANCE = 6; // should always be 6 inches from wall
+const byte RANGE = 1; // can be + or - 1 inch from WALL_DISTANCE
+const byte COURSE_WIDTH = 28;
+
+/*byte forward_count = 0;
+byte left_count = 0;
+byte right_count = 0;*/
+// count variables increment for consecutive times where
+// forward/left/right is < safe value
+// put in reverse when count is high enough
+
+float scan_time = 1.5;
+
+// timer interrupt occurs every 0.01s
+// decrement time variables to have 
+ISR(TIMER2_OVF_vect) {
+  if(scan_time < 0.01) // bring back to 1.5 s when 0
+  {
+    Serial.println("hi");
+    scan_time = 1.5; 
+    scan();
+  }
+
+  scan_time = scan_time - 0.01;
+  TCNT2 = TM2_COUNT;
+}
+
+void scan() {
+  currentAngle = servo.read();
+  //Serial.println(currentAngle);
+  if(currentAngle == 90)
+  { // if sensor is currently facing forward,
+    // scan forward then turn left and scan
+    forward = sensor.measureInches();
+    servo.write(180);
+    delay(625); // wait for servo to turn
+    left = sensor.measureInches();
+  }
+  else
+  { // if sensor is currently facing left,
+    // scan left then turn forward and scan
+    left = sensor.measureInches();
+    servo.write(90);
+    delay(625); // wait for servo to turn
+    forward = sensor.measureInches();
+  }
+
+  if(forward < 12) // forward < 12 inches
+  {
+    forward_count++;
+    if(forward_count > 2 || forward < 3) state = Reverse;
+    // ^ put robot in reverse if it is stuck or close to wall
+    else if(left < COURSE_WIDTH) state = TurnRight;
+    // ^ if approaching forward wall and left wall exists,
+    // turn right
+    else state = TurnLeft;
+    // ^ if approaching forward wall and no close left wall,
+    // turn left
+  }
+
+  else if(left < WALL_DISTANCE - RANGE)
+  {
+    forward_count = 0;
+    // ^ should not be stuck in forward direction
+    state = CorrectRight;
+    // if too close to left wall, correct to right
+  }
+
+  else if(left > WALL_DISTANCE + RANGE)
+  {
+    forward_count = 0;
+    // ^ should not be stuck in forward direction
+    state = CorrectLeft;
+    // if too far from left wall, correct to left
+  }
+
+  else if((left > COURSE_WIDTH && forward > COURSE_WIDTH) ||
+          (left == 0 && forward == 0))
+  {
+    state = Stop;
+    // ^ stop if both left and forward
+    // are greater than the course width
+    // or if both are too far (returns 0)
+  }
+
+  else 
+  {
+    forward_count = 0; // not stuck
+    state = MoveForward;
+  }
+}
+
+
+
+
+// Scratch Section ************************************
+//  if(IrReceiver.decode()){
+//    if(IrReceiver.decodedIRData.command == change){
+//      auto_mode = (auto_mode == true) ? false : true; 
+//      // stop robot for a second
+//       leftMotor.run(MotorStop);
+//       rightMotor.run(MotorStop);
+//       delay(1200);
+//    }
+//    IrReceiver.resume();
+//  }
